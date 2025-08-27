87d01fde32e4179697eabeb45be87cca3c9faa32
Drake KG <92551290+drakeKG@users.noreply.github.com>
2025-08-27 13:14:18 +0200

    Updated run_experiment.sh
      -- Run all experiments and save results.

--- sylva/run_experiment.sh
+++ sylva/run_experiment.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
+REPO_PATH=$(pwd)
+echo "Repo path: ${REPO_PATH}"
+
+logfile="${REPO_PATH}/logs/running.log"
+errfile="${REPO_PATH}/logs/error.log"
+
+mkdir -p "${REPO_PATH}/logs"
+
+# Function to run command with redirected logs
 function nullify() {
-  "$@" >/dev/null 2>&1
+  "$@" >"${logfile}" 2>"${errfile}"
 }
 
-
 echo "running experiment 1"
-for i in 16 32 64 128
-do
-    echo "parameter (address pattern) = $i"
-    nullify pushd examples/copy
-    nullify bash ./run.sh $i
-    nullify popd
+for i in 16 32 64 128; do
+  echo "parameter (address pattern) = $i" | tee -a logs/Exp1.log
+  nullify pushd examples/copy
+  nullify bash ./run.sh $i
+  nullify popd
+  for j in {1..8}; do
     python3 -m tb.testcase -n exp1 -p $i
-    ./run.sh 2>/dev/null | grep "Elapsed time"
+    ./run.sh > running.log
+    grep "Elapsed time" running.log | tee -a logs/Exp1.log
+  done
 done
 
-echo ""
 echo "running experiment 2"
 nullify pushd examples/copy
 nullify bash ./run.sh 32
 nullify popd
-for i in 2 4 8 16
-do
-    echo "number of nodes = $i"
+for i in 2 4 8 16; do
+  echo "number of nodes = $i" | tee -a logs/Exp2.log
+  for j in {1..8}; do
     python3 -m tb.testcase -n exp2 -p $i
-    ./run.sh 2>/dev/null | grep "Elapsed time"
+    ./run.sh > running.log
+    grep "Elapsed time" running.log | tee -a logs/Exp2.log
+  done
 done
 
-# default setting: set token to 16 
-nullify pushd examples/copy
-nullify bash ./run.sh 16
-nullify popd 
+# # default setting: set token to 16
+nullify pushd examples/sobel
+nullify bash ./run.sh
+nullify popd
+python3 -m tb.testcase -n sobel
+./run.sh > running.log
+grep "Elapsed time" running.log | tee -a logs/sobel.log
+
+nullify pushd examples/lenet5
+nullify bash ./run.sh
+nullify popd
+python3 -m tb.testcase -n lenet5
+./run.sh > running.log
+grep "Elapsed time" running.log | tee -a logs/lenet5.log
